401498c42b1945a202a89a31adba5c2e29c59e4a
lizzethgd <lizzethgd@gmail.com>
2020-12-04 02:36:46 +0200

    package.json modified 4

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.join(__dirname, "client", "build")))
+app.use(express.static(path.join(__dirname, '../client/build')))
 
 // initialize routes
 app.use('/api', require('./routes/api'));
@@ -34,7 +34,7 @@ mongoose.connect(process.env.MONGODB_URI, {
 })
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build/index.html'))
+    res.sendFile(path.join(__dirname, '../client/build/index.html'))
   })
 
 // listen for requests
